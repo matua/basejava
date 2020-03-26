ef0c58658d84c67ccde00aa4e08a802d429d6433
Georgii Matua <matuageorge@gmail.com>
2020-03-26 10:10:15 +0300

    Full implementation of the class

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,63 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume toFind = null;
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].toString().equals(uuid)) {
+                toFind = storage[i];
+                break;
+            }
+        }
+        return toFind;
     }
 
     void delete(String uuid) {
+        int count = 0;
+        for (Resume resume : storage) {
+            if (resume.toString().equals(uuid)) {
+                resume = null;
+                break;
+            }
+            count++;
+        }
+
+        if (storage.length - 1 - count >= 0)
+            System.arraycopy(storage, count + 1, storage, count, storage.length - 1 - count);
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] noNullStorage = new Resume[size()];
+
+        if (size() >= 0) System.arraycopy(storage, 0, noNullStorage, 0, size());
+        return noNullStorage;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (Resume resume : storage) {
+            if (resume == null) {
+                break;
+            }
+            count++;
+        }
+        return count;
     }
 }
