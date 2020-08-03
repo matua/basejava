1255aacfdbc0752aa349781e1d9441c8e9e1384b
Georgii Matua <matuageorge@gmail.com>
2020-08-03 12:34:03 +0300

    Lesson 11
    1. extracted a method lockingThread() to avoid duplicating of the code
    2. added messages to show the state of locking the objects

--- src/main/java/com/matuageorge/webapp/Deadlock.java
+++ src/main/java/com/matuageorge/webapp/Deadlock.java
@@ -5,28 +5,23 @@ public class Deadlock {
         A a = new A();
         B b = new B();
 
+        lockingThread(a, b);
+
+        lockingThread(b, a);
+    }
+
+    private static void lockingThread(Object a, Object b) {
         new Thread(() -> {
             synchronized (a) {
+                System.out.format("%s locking %s%n", Thread.currentThread().getName(), a.getClass().getSimpleName());
                 try {
                     Thread.sleep(1);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                System.out.format("%s waiting to lock %s%n", Thread.currentThread().getName(), b.getClass().getSimpleName());
                 synchronized (b) {
-
-                }
-            }
-        }).start();
-
-        new Thread(() -> {
-            synchronized (b) {
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (a) {
-
+                    System.out.format("%s locking %s and %s%n", Thread.currentThread().getName(), a.getClass().getSimpleName(), b.getClass().getSimpleName());
                 }
             }
         }).start();
