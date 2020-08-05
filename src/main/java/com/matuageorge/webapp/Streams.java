39bad48e120aaff11fe47cca9740fabe85ceceb5
Georgii Matua <matuageorge@gmail.com>
2020-08-05 15:16:23 +0300

    Lesson 12
    1. created List<Integer> oddOrEven(List<Integer> integers) in Streams class and a test for it in StreamsTest

--- src/main/java/com/matuageorge/webapp/Streams.java
+++ src/main/java/com/matuageorge/webapp/Streams.java
@@ -1,11 +1,17 @@
 package com.matuageorge.webapp;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /*реализовать метод через стрим int minValue(int[] values).
         Метод принимает массив цифр от 1 до 9, надо выбрать уникальные и вернуть минимально возможное число,
         составленное из этих уникальных цифр. Не использовать преобразование в строку и обратно. Например {1,2,3,3,2,3}
         вернет 123, а {9,8} вернет 89*/
+
+/*реализовать метод List<Integer> oddOrEven(List<Integer> integers) если сумма всех чисел нечетная - удалить все нечетные,
+        если четная - удалить все четные. Сложность алгоритма должна быть O(N). Optional - решение в один стрим.*/
+
 public class Streams {
     public static int minValue(int[] values) {
         return Arrays.stream(values)
@@ -13,5 +19,22 @@ public class Streams {
                 .sorted()
                 .reduce((n1, n2) -> n1 * 10 + n2)
                 .getAsInt();
+    }
+
+    public static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers.stream().mapToInt(Integer::intValue)
+                .sum();
+
+        if (sum % 2 != 0) {
+            return integers
+                    .stream()
+                    .filter(n -> n % 2 == 0)
+                    .collect(Collectors.toList());
+        } else {
+            return integers
+                    .stream()
+                    .filter(n -> n % 2 != 0)
+                    .collect(Collectors.toList());
+        }
     }
 }
--- src/test/java/com/matuageorge/webapp/StreamsTest.java
+++ src/test/java/com/matuageorge/webapp/StreamsTest.java
@@ -2,13 +2,58 @@ package com.matuageorge.webapp;
 
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static com.matuageorge.webapp.Streams.minValue;
+import static com.matuageorge.webapp.Streams.oddOrEven;
 
 public class StreamsTest extends TestCase {
 
     public void testMinValue() {
         assertEquals(89, minValue(new int[]{9, 8}));
         assertEquals(123, minValue(new int[]{1, 2, 3, 3, 2, 3}));
-        assertEquals(1246, minValue(new int[]{0, 1, 6, 4, 2, 4}));
+        assertEquals(12359, minValue(new int[]{3, 2, 0, 5, 2, 9, 1, 0}));
+    }
+
+    public void testOddOrEven() {
+        List<Integer> many = new ArrayList<>();
+        for (int i = 0; i < 1_000_000; i++) {
+            many.add((int) (Math.random() * 10));
+        }
+
+        List<Integer> tooMany = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            tooMany.add((int) (Math.random() * 10));
+        }
+
+        List<Integer> extremelyMany = new ArrayList<>();
+        for (int i = 0; i < 100_000_000; i++) {
+            extremelyMany.add((int) (Math.random() * 10));
+        }
+
+        List<Integer> expectedResult1 = Arrays.asList(8);
+        List<Integer> expectedResult2 = Arrays.asList(1, 3, 3, 3);
+        List<Integer> expectedResult3 = Arrays.asList(3, 5, 9, 1);
+        assertEquals(expectedResult1, oddOrEven(Arrays.asList(9, 8)));
+        assertEquals(expectedResult2, oddOrEven(Arrays.asList(1, 2, 3, 3, 2, 3)));
+        assertEquals(expectedResult3, oddOrEven(Arrays.asList(3, 2, 0, 5, 2, 9, 1, 0)));
+
+
+        long start = System.currentTimeMillis();
+        oddOrEven(many);
+        System.out.format("Many: %d%n", System.currentTimeMillis() - start);
+
+
+        long start1 = System.currentTimeMillis();
+        oddOrEven(tooMany);
+        System.out.format("Too many: %d%n", System.currentTimeMillis() - start1);
+
+
+        long start2 = System.currentTimeMillis();
+        oddOrEven(extremelyMany);
+        System.out.format("Extremely many: %d%n", System.currentTimeMillis() - start2);
+
     }
 }
