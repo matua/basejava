6640fc6df0b081a9080b0949df7f586852ad8fca
Georgii Matua <matuageorge@gmail.com>
2020-09-23 16:55:10 +0300

    Lesson 15
    1. HTML rendering of the resumes table implemented in resumeServlet

--- src/main/java/resumeServlet.java
+++ src/main/java/resumeServlet.java
@@ -1,18 +1,44 @@
+import com.matuageorge.webapp.Config;
+import com.matuageorge.webapp.model.Resume;
+import com.matuageorge.webapp.storage.Storage;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class resumeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
-        String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + "!");
+
+        Storage resumeStorage = Config.get().getSqlStorage();
+        List<Resume> resumes = resumeStorage.getAllSorted();
+
+        String head = "<head>\n" +
+                "<link rel=\"stylesheet\" href=\"css/tableFormat.css\"/>" +
+                "</head>";
+
+        String tableHeader = "<body><table class=\"paleBlueRows\">\n" +
+                "<tr>\n" +
+                "<th>Resume UUID</th>\n" +
+                "<th>Full Name</th>\n" +
+                "  </tr>\n";
+        String tableFooter = "</table><body>";
+
+        StringBuilder cells = new StringBuilder();
+
+        resumes.forEach(resume -> cells.append("<tr>" +
+                "<td>")
+                .append(resume.getUuid())
+                .append("</td><td>")
+                .append(resume.getFullName())
+                .append("</td></tr>"));
+        response.getWriter().write(head + tableHeader + cells + tableFooter);
     }
 }
